de313a2fadeef36302bb43ecf41a44f254441ac5
SARAFF <office@saraff.ru>
2018-07-28 17:20:43 +0300

    Изменен номер версии.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -62,5 +62,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.534")]
-[assembly: AssemblyFileVersion("1.0.2.534")]
+[assembly: AssemblyVersion("1.0.3.632")]
+[assembly: AssemblyFileVersion("1.0.3.632")]
